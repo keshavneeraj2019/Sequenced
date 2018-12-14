12029c42fc175995ce3ae3bbe24d09eb2fc9ab20
keshavneeraj2019 <42677916+keshavneeraj2019@users.noreply.github.com>
2018-12-13 19:38:44 -0800

    Update main.java

--- main.java
+++ main.java
@@ -1,55 +1,61 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("First 10 Perfect Squares are");
-        for(int n = 1; n <=10; n++) {
+        System.out.println("The first 10 Perfect Squares are");
+        int n = 0;
+        while (n < 10) {
+            n++;
             int square = n * n;
-            System.out.printf("%s ", square);
+            System.out.printf("%d ", square);
         }
         System.out.println();
 
 
-        System.out.println("First 10 Perfect Cubes are ");
-        for(int n = 1; n <=10; n++) {
+        System.out.println("The first 10 Perfect Cubes are ");
+        n = 0;
+        while (n < 10) {
+            n++;
             int cube = n * n * n;
-            System.out.printf("%s ", cube);
+            System.out.printf("%d ", cube);
         }
         System.out.println();
 
-        System.out.println("First 10 Fibonacci Numbers are ");
-        int[]fibonacci = new int[10];
-        fibonacci[0] = 1;
-        fibonacci[1] = 2;
-        for(int i = 2; i < 10; i++) {
-            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-        }
-        for(int i = 0; i < fibonacci.length; i++) {
-            System.out.printf("%d ", fibonacci[i]);
-        }
-        System.out.println();
-
-        System.out.println("First 10 Prime Numbers are");
-        for (int n = 2; n < 30; n++) {
-            boolean num = false;
-            for (int i = 2; i <= n / 2; ++i) {
-                if (n % i == 0) {
-                    num = true;
-                    break;
-                }
-            }
-            if (!num)
-                System.out.printf("%d ", n);
+        System.out.println("The first 10 Fibonacci Numbers are ");
+        n = 0;
+        int n1 = 0;
+        int n2 = 1;
+        int n3;
+        while (n <= 10) {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            n++;
+            System.out.printf("%d ", n3);
         }
         System.out.println();
 
-        System.out.println("First 10 Triangle Numbers are");
-        int[]triangles = new int[10];
-        triangles[0] = 1;
-        for(int i = 1; i < triangles.length; i++) {
-            triangles[i] = triangles[i - 1] + i + 1;
-        }
-        for(int i = 0; i < triangles.length; i++) {
-            System.out.printf("%d ", triangles[i]);
+        System.out.println("The first 10 Prime Numbers are");
+        int i = 2;
+        while (i < 30) {
+            boolean flag = false;
+            n = 2;
+            while (n <= (i / 2)) {
+                if (i % n == 0) {
+                    flag = true;
+                    break; }
+                n++; }
+            if (!flag) {
+                System.out.printf("%d ", i); }
+            i++;
+        } System.out.println();
+
+
+        System.out.println("The first 10 Triangle Numbers are");
+        n = 1;
+        while (n <= 10) {
+            int triangle = (n * (n + 1) / 2);
+            System.out.printf("%d ", triangle);
+            n++;
         }
 
     }
